b24369623662f4be34ff03a02df6ba3f2861255b
h5b24321 <dolandj969@gmail.com>
2026-01-13 15:47:44 +0300

    Refactor Form1.cs to remove shortcut creation
    Removed shortcut creation functionality and related comments. Updated comments for clarity.

--- Form1.cs
+++ Form1.cs
@@ -3,15 +3,14 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using System.IO; // Kısayol yolu için gerekli
-using IWshRuntimeLibrary; // COM Referansını eklemiş olmalısın
 
 namespace crosshair
 {
     public partial class Form1 : Form
     {
+        // Ayarlar
         public static Color CColor = Color.Lime;
-        public static int CSize = 12, CThick = 2, CGap = 5, CMode = 0; 
+        public static int CSize = 12, CThick = 2, CGap = 5, CMode = 0;
         public static bool COutline = true;
 
         [DllImport("user32.dll")]
@@ -21,8 +20,7 @@ namespace crosshair
 
         public Form1()
         {
-            InitializeComponent(); // Eğer designer kullanıyorsan bu kalmalı
-            
+            // Formu tam ekran, şeffaf ve en üstte yapar
             this.FormBorderStyle = FormBorderStyle.None;
             this.TopMost = true;
             this.BackColor = Color.Black;
@@ -30,28 +28,8 @@ namespace crosshair
             this.WindowState = FormWindowState.Maximized;
             this.ShowInTaskbar = false;
 
-            // Kısayol oluşturma fonksiyonunu burada çağırıyoruz
-            try { CreateShortcut(); } catch { }
-
-            new FormAyarlar().Show();
-        }
-
-        private void CreateShortcut()
-        {
-            // Masaüstü yolunu alıyoruz
-            string shortcutPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Crosshair.lnk");
-
-            // Eğer kısayol yoksa oluştur
-            if (!System.IO.File.Exists(shortcutPath))
-            {
-                WshShell shell = new WshShell();
-                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
-
-                shortcut.Description = "Zeki'nin Crosshair Uygulaması";
-                shortcut.TargetPath = Application.ExecutablePath; 
-                shortcut.WorkingDirectory = Application.StartupPath;
-                shortcut.Save();
-            }
+            // Ayarlar formu projedeyse açar
+            try { new FormAyarlar().Show(); } catch { }
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -63,22 +41,17 @@ namespace crosshair
             using (Pen p = new Pen(CColor, CThick))
             using (Pen s = new Pen(Color.FromArgb(160, 0, 0, 0), CThick + 2))
             {
-                if (CMode == 0 || CMode == 4)
+                if (CMode == 0 || CMode == 4) // Artı veya T-Style
                 {
                     if (CMode != 4) DrawL(e.Graphics, p, s, x, y - CGap, x, y - CGap - CSize);
                     DrawL(e.Graphics, p, s, x, y + CGap, x, y + CGap + CSize);
                     DrawL(e.Graphics, p, s, x - CGap, y, x - CGap - CSize, y);
                     DrawL(e.Graphics, p, s, x + CGap, y, x + CGap + CSize, y);
                 }
-                else if (CMode == 1)
+                else if (CMode == 1) // Nokta
                 {
                     e.Graphics.FillEllipse(Brushes.Black, x - CThick - 1, y - CThick - 1, (CThick + 1) * 2, (CThick + 1) * 2);
                     e.Graphics.FillEllipse(new SolidBrush(CColor), x - CThick, y - CThick, CThick * 2, CThick * 2);
-                }
-                else if (CMode == 2)
-                {
-                    e.Graphics.DrawRectangle(s, x - CGap, y - CGap, CGap * 2, CGap * 2);
-                    e.Graphics.DrawRectangle(p, x - CGap, y - CGap, CGap * 2, CGap * 2);
                 }
             }
         }
@@ -89,10 +62,11 @@ namespace crosshair
             g.DrawLine(p, x1, y1, x2, y2);
         }
 
-        protected override void OnLoad(EventArgs e) 
-        { 
-            base.OnLoad(e); 
-            SetWindowLong(this.Handle, -20, GetWindowLong(this.Handle, -20) | 0x80000 | 0x20); 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            // Mouse tıklamalarını arkadaki oyuna geçiren kod
+            SetWindowLong(this.Handle, -20, GetWindowLong(this.Handle, -20) | 0x80000 | 0x20);
         }
     }
 }
